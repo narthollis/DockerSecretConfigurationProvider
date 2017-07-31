86e6e36d420be9910614c592e921b98528d75ea2
Nicholas Steicke <nicholas.steicke@narthollis.net>
2017-07-31 20:48:10 +0930

    Initial Commit of the Docket Secret Confiugration Provider

---
+++ DockerSecretConfigurationProvider/DockerSecretConfigurationSource.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DockerSecretConfigurationProvider
+{
+    public class DockerSecretConfigurationSource : IConfigurationSource
+    {
+        public string Prefix { get; set; }
+        public string Path { get; set; }
+        
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new DocketSecretConfigurationProvider(Prefix, Path);
+        }
+    }
+}
---
+++ DockerSecretConfigurationProvider/DockerSecretExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DockerSecretConfigurationProvider
+{
+    public static class DockerSecretExtensions
+    {
+        public static IConfigurationBuilder AddDockerSecrets(this IConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Add(new DockerSecretConfigrationSource());
+            return configurationBuilder;
+        }
+
+        public static IConfigurationBuilder AddDockerSecrets(this IConfigurationBuilder configurationBuilder, string prefix)
+        {
+            configurationBuilder.Add(new DockerSecretConfigrationSource { Prefix = prefix });
+            return configurationBuilder;
+        }
+
+        public static IConfigurationBuilder AddDockerSecrets(this IConfigurationBuilder configurationBuilder, string prefix, string path)
+        {
+            configurationBuilder.Add(new DockerSecretConfigrationSource { Prefix = prefix, Path = path });
+            return configurationBuilder;
+        }
+    }
+}
